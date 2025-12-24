ae1f1f7da6dbf2f8ce1f86366c8eaef5102750fe
lewixprime <163653288+lewixprime@users.noreply.github.com>
2025-12-24 20:42:37 +0500

    Update run_webapp.py

--- run_webapp.py
+++ run_webapp.py
@@ -1,12 +1,18 @@
 # run_webapp.py
 import uvicorn
 import os
+import sys
+
+# Добавляем корень проекта в путь
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, BASE_DIR)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
-        "webapp.app:app",
+        "app:app",
         host="0.0.0.0",
         port=port,
-        reload=False
+        reload=False,
+        log_level="info"
     )
